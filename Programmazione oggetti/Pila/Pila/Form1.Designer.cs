018ffd7a15ae8a7762d735f86206652fe2ea519d
Stud. Simone Zerbin <s.zerbin.1817@vallauri.net>
2021-11-20 09:01:21 +0100

    Pile, code, dictionary

---
+++ Programmazione oggetti/Pila/Pila/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Pila
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodice = new System.Windows.Forms.TextBox();
+            this.txtTara = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.btnInserisci = new System.Windows.Forms.Button();
+            this.btnMostra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codice";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(117, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Peso";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(117, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tara";
+            // 
+            // txtCodice
+            // 
+            this.txtCodice.Location = new System.Drawing.Point(192, 95);
+            this.txtCodice.Name = "txtCodice";
+            this.txtCodice.Size = new System.Drawing.Size(100, 20);
+            this.txtCodice.TabIndex = 3;
+            // 
+            // txtTara
+            // 
+            this.txtTara.Location = new System.Drawing.Point(192, 143);
+            this.txtTara.Name = "txtTara";
+            this.txtTara.Size = new System.Drawing.Size(100, 20);
+            this.txtTara.TabIndex = 4;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(192, 192);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 5;
+            // 
+            // btnInserisci
+            // 
+            this.btnInserisci.Location = new System.Drawing.Point(177, 233);
+            this.btnInserisci.Name = "btnInserisci";
+            this.btnInserisci.Size = new System.Drawing.Size(75, 46);
+            this.btnInserisci.TabIndex = 6;
+            this.btnInserisci.Text = "Inserisci container";
+            this.btnInserisci.UseVisualStyleBackColor = true;
+            this.btnInserisci.Click += new System.EventHandler(this.btnInserisci_Click);
+            // 
+            // btnMostra
+            // 
+            this.btnMostra.Location = new System.Drawing.Point(177, 303);
+            this.btnMostra.Name = "btnMostra";
+            this.btnMostra.Size = new System.Drawing.Size(75, 44);
+            this.btnMostra.TabIndex = 7;
+            this.btnMostra.Text = "Mostra i container";
+            this.btnMostra.UseVisualStyleBackColor = true;
+            this.btnMostra.Click += new System.EventHandler(this.btnMostra_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMostra);
+            this.Controls.Add(this.btnInserisci);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtTara);
+            this.Controls.Add(this.txtCodice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodice;
+        private System.Windows.Forms.TextBox txtTara;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Button btnInserisci;
+        private System.Windows.Forms.Button btnMostra;
+    }
+}
+
