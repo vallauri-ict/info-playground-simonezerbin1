4da6a1085d15a20bb231beb513ab24333fdd66b8
Stud. Simone Zerbin <s.zerbin.1817@vallauri.net>
2022-01-15 11:06:04 +0100

    Es Regex

---
+++ Programmazione oggetti/Compito RegEx/Compito RegEx/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Compito_RegEx
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCognome = new System.Windows.Forms.TextBox();
+            this.txtEtà = new System.Windows.Forms.TextBox();
+            this.txtCittà = new System.Windows.Forms.TextBox();
+            this.txtCap = new System.Windows.Forms.TextBox();
+            this.txtCF = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnInserisciDati = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOME:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ETÀ: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "COGNOME";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(94, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ELENCO E-MAIL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(94, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "CITTÀ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(94, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "CAP:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(94, 368);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "PASSWORD";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(94, 327);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "USERNAME";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(94, 285);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "CODICE FISCALE";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(206, 37);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(196, 20);
+            this.txtNome.TabIndex = 9;
+            // 
+            // txtCognome
+            // 
+            this.txtCognome.Location = new System.Drawing.Point(206, 80);
+            this.txtCognome.Name = "txtCognome";
+            this.txtCognome.Size = new System.Drawing.Size(196, 20);
+            this.txtCognome.TabIndex = 10;
+            // 
+            // txtEtà
+            // 
+            this.txtEtà.Location = new System.Drawing.Point(206, 122);
+            this.txtEtà.Name = "txtEtà";
+            this.txtEtà.Size = new System.Drawing.Size(196, 20);
+            this.txtEtà.TabIndex = 11;
+            // 
+            // txtCittà
+            // 
+            this.txtCittà.Location = new System.Drawing.Point(206, 202);
+            this.txtCittà.Name = "txtCittà";
+            this.txtCittà.Size = new System.Drawing.Size(196, 20);
+            this.txtCittà.TabIndex = 12;
+            // 
+            // txtCap
+            // 
+            this.txtCap.Location = new System.Drawing.Point(206, 161);
+            this.txtCap.Name = "txtCap";
+            this.txtCap.Size = new System.Drawing.Size(196, 20);
+            this.txtCap.TabIndex = 13;
+            // 
+            // txtCF
+            // 
+            this.txtCF.Location = new System.Drawing.Point(206, 282);
+            this.txtCF.Name = "txtCF";
+            this.txtCF.Size = new System.Drawing.Size(196, 20);
+            this.txtCF.TabIndex = 14;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(206, 242);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(196, 20);
+            this.txtEmail.TabIndex = 15;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(206, 324);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(196, 20);
+            this.txtUsername.TabIndex = 16;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(206, 365);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(196, 20);
+            this.txtPassword.TabIndex = 18;
+            // 
+            // btnInserisciDati
+            // 
+            this.btnInserisciDati.Location = new System.Drawing.Point(591, 122);
+            this.btnInserisciDati.Name = "btnInserisciDati";
+            this.btnInserisciDati.Size = new System.Drawing.Size(115, 54);
+            this.btnInserisciDati.TabIndex = 19;
+            this.btnInserisciDati.Text = "Inserisci dati";
+            this.btnInserisciDati.UseVisualStyleBackColor = true;
+            this.btnInserisciDati.Click += new System.EventHandler(this.btnInserisciDati_Click);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 478);
+            this.Controls.Add(this.btnInserisciDati);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtCF);
+            this.Controls.Add(this.txtCap);
+            this.Controls.Add(this.txtCittà);
+            this.Controls.Add(this.txtEtà);
+            this.Controls.Add(this.txtCognome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCognome;
+        private System.Windows.Forms.TextBox txtEtà;
+        private System.Windows.Forms.TextBox txtCittà;
+        private System.Windows.Forms.TextBox txtCap;
+        private System.Windows.Forms.TextBox txtCF;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnInserisciDati;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
+
